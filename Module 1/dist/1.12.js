e3c88772245b1b4553ce5b20687f653ee387b98c
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-04-30 21:30:32 +0600

    Never,Unknown And Nullable Type

---
+++ Module 1/dist/1.12.js
@@ -0,0 +1,28 @@
+"use strict";
+/*
+ * Title: Never,Unknown And Nullable Type
+ * Description:
+ * Author: Md. Atikur Rahaman
+ * Date: 30-04-2024
+ */
+{
+    const searchName = (value) => {
+        if (value) {
+            console.log("Searching");
+        }
+        else {
+            console.log("Nothing to search");
+        }
+    };
+    searchName(null);
+    const getSpeedPerMeterSecond = (value) => {
+        if (typeof value === "number") {
+            return value / 1000;
+        }
+        else {
+            return "Invalid input";
+        }
+    };
+    const result = getSpeedPerMeterSecond("500");
+    console.log(result);
+}
